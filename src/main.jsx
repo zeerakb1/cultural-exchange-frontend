a95eb2fab5bb8ee7914199176e6325acfe9c377d
zeerakb1 <zeerakbabar@gmail.com>
2024-06-29 18:39:11 -0700

    cultural exchange frontend

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+
+if (process.env.NODE_ENV !== 'production') disableReactDevTools();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
--- src/url.js
+++ src/url.js
@@ -1,2 +1,4 @@
-export const URL = "http://localhost:5500";
-export const IF = "http://localhost:5500/images/";
+export const URL = "https://cultural-api.onrender.com";
+// export const URL = "http://localhost:5500";
+export const IF = "https://cultural-api.onrender.com/images/";
+// export const IF = "http://localhost:5500/images/";
